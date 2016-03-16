173ca597dea64a5631eafa0ad97e562674f5b32d
Miszel66 <nolanb.o.s@gmail.com>
2016-03-16 20:39:39 +0100

    dodałem getery i setery (metody putCostam) a takze toString

--- tydzien3/src/tydzien3/ksiazkaTelefoniczna.java
+++ tydzien3/src/tydzien3/ksiazkaTelefoniczna.java
@@ -3,22 +3,33 @@ package tydzien3;
 
 public class ksiazkaTelefoniczna {
 	private String imie;
+	private String nazwisko;
 	private int numer;
 	private String miasto;
 	private String ulica;
 	private int numerDomu;
 	
 	
-	public ksiazkaTelefoniczna(String imie, int numer,String miasto, String ulica, int numerDomu)
+	public ksiazkaTelefoniczna(String imie, String nazwisko, int numer,String miasto, String ulica, int numerDomu)
 	{
 		this.imie = imie;
+		this.nazwisko = nazwisko;
 		this.numer = numer;
 		this.miasto = miasto;
 		this.ulica = ulica;
+		this.numerDomu = numerDomu;
+	}
+	public String toString()
+	{
+		return " " + this.imie +" "+ this.nazwisko +" "+ this.numer +" "+ this.miasto +" "+ this.ulica +" "+ this.numerDomu +" ";
 	}
 	public String getImie()
 	{
 		return this.imie;
+	}
+	public String getNazwisko()
+	{
+		return this.nazwisko;
 	}
 	public int getNumer()
 	{
@@ -36,5 +47,32 @@ public class ksiazkaTelefoniczna {
 	{
 		return this.numerDomu;
 	}
+	
+	public void putImie(String imie)
+	{
+		this.imie = imie;
+	}
+	public void putNazwisko(String nazwisko)
+	{
+		this.nazwisko = nazwisko;
+		
+	}
+	public void putNumer(int numer)
+	{
+		this.numer = numer;
+	}
+	public void putMiasto(String miasto)
+	{
+		this.miasto = miasto;
+	}
+	public void putUlica(String ulica)
+	{
+		this.ulica = ulica;
+	}
+	public void putNrDomu(int nrDomu)
+	{
+		this.numerDomu = nrDomu;
+	}
+	
 
 }
--- tydzien3/src/tydzien3/tydzien3.java
+++ tydzien3/src/tydzien3/tydzien3.java
@@ -1,14 +1,30 @@
 package tydzien3;
-import java.util.Vector;
 
 public class tydzien3 {
 
 	public static void main(String[] args) {
-	Vector<ksiazkaTelefoniczna> vec = new Vector<ksiazkaTelefoniczna>();
-	ksiazkaTelefoniczna k1 = new ksiazkaTelefoniczna("jan",666,"lodz","gdanska",25);
-	vec.add(k1);
-	System.out.println(vec.get(0).getImie());
-
+		ksiazkaTelefoniczna [] ksiazka = new ksiazkaTelefoniczna[10];
+		Object [] object = new Object[10];
+		for(int i=0;i<ksiazka.length;i++)
+		{
+			ksiazka[i] = new ksiazkaTelefoniczna("Jan", "Kowalski", 123123323, "Lodz", "Gdanska", 22);
+		}
+		for(int i=0;i<ksiazka.length;i++)
+		{
+			System.out.println(ksiazka[i].toString());
+		}
+		
+		for(int i=0;i<ksiazka.length;i++)
+		{
+			object[i] = ksiazka[i];
+		}
+		//cos sobie probowa³em mniejsza o to :D
+		System.out.println("===============================================");
+		
+		for(int i=0;i<object.length;i++)
+		{
+			System.out.println(object[i].toString());
+		}
 	}
 
 }
