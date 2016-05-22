1ad9765af7bf1bed2d39b3a012603b7891a26dd1
MiszelHub <nolanb.o.s@gmail.com>
2016-05-22 21:56:38 +0200

    dodalem comboboxa i menu w oddzielnych klasach

--- zadanie_9_10/src/zadanie_9_10/Program.java
+++ zadanie_9_10/src/zadanie_9_10/Program.java
@@ -1,100 +1,14 @@
 package zadanie_9_10;
 import java.awt.BorderLayout;
 import java.awt.Container;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
 
-import javax.swing.*;     
+import javax.swing.*;
 
 public class Program {
-	 
+
 	  JTextArea output;
 	    JScrollPane scrollPane;
 
-	    public JMenuBar createMenuBar() {
-	        JMenuBar menuBar;
-	        JMenu menu, submenu;
-	        JMenuItem menuItem;
-	        JRadioButtonMenuItem rbMenuItem;
-	        JCheckBoxMenuItem cbMenuItem;
-
-	        //Create the menu bar.
-	        menuBar = new JMenuBar();
-
-	        //Build the first menu.
-	        menu = new JMenu("A Menu");
-	        menu.setMnemonic(KeyEvent.VK_A);
-	        menu.getAccessibleContext().setAccessibleDescription(
-	                "The only menu in this program that has menu items");
-	        menuBar.add(menu);
-
-	        //a group of JMenuItems
-	        menuItem = new JMenuItem("A text-only menu item",
-	                                 KeyEvent.VK_T);
-	        //menuItem.setMnemonic(KeyEvent.VK_T); //used constructor instead
-	        menuItem.setAccelerator(KeyStroke.getKeyStroke(
-	                KeyEvent.VK_1, ActionEvent.ALT_MASK));
-	        menuItem.getAccessibleContext().setAccessibleDescription(
-	                "This doesn't really do anything");
-	        menu.add(menuItem);
-
-	        ImageIcon icon = createImageIcon("images/middle.gif");
-	        menuItem = new JMenuItem("Both text and icon", icon);
-	        menuItem.setMnemonic(KeyEvent.VK_B);
-	        menu.add(menuItem);
-
-	        menuItem = new JMenuItem(icon);
-	        menuItem.setMnemonic(KeyEvent.VK_D);
-	        menu.add(menuItem);
-
-	        //a group of radio button menu items
-	        menu.addSeparator();
-	        ButtonGroup group = new ButtonGroup();
-
-	        rbMenuItem = new JRadioButtonMenuItem("A radio button menu item");
-	        rbMenuItem.setSelected(true);
-	        rbMenuItem.setMnemonic(KeyEvent.VK_R);
-	        group.add(rbMenuItem);
-	        menu.add(rbMenuItem);
-
-	        rbMenuItem = new JRadioButtonMenuItem("Another one");
-	        rbMenuItem.setMnemonic(KeyEvent.VK_O);
-	        group.add(rbMenuItem);
-	        menu.add(rbMenuItem);
-
-	        //a group of check box menu items
-	        menu.addSeparator();
-	        cbMenuItem = new JCheckBoxMenuItem("A check box menu item");
-	        cbMenuItem.setMnemonic(KeyEvent.VK_C);
-	        menu.add(cbMenuItem);
-
-	        cbMenuItem = new JCheckBoxMenuItem("Another one");
-	        cbMenuItem.setMnemonic(KeyEvent.VK_H);
-	        menu.add(cbMenuItem);
-
-	        //a submenu
-	        menu.addSeparator();
-	        submenu = new JMenu("A submenu");
-	        submenu.setMnemonic(KeyEvent.VK_S);
-
-	        menuItem = new JMenuItem("An item in the submenu");
-	        menuItem.setAccelerator(KeyStroke.getKeyStroke(
-	                KeyEvent.VK_2, ActionEvent.ALT_MASK));
-	        submenu.add(menuItem);
-
-	        menuItem = new JMenuItem("Another item");
-	        submenu.add(menuItem);
-	        menu.add(submenu);
-
-	        //Build second menu in the menu bar.
-	        menu = new JMenu("Another Menu");
-	        menu.setMnemonic(KeyEvent.VK_N);
-	        menu.getAccessibleContext().setAccessibleDescription(
-	                "This menu does nothing");
-	        menuBar.add(menu);
-
-	        return menuBar;
-	    }
 	private static void createAndShowGUI() {
 	        //Create and set up the window.
 	        JFrame frame = new JFrame("HelloWorldSwing");
@@ -109,8 +23,10 @@ public class Program {
 	        frame.setVisible(true);
 	        //Create and set up the content pane.
 	        Program demo = new Program();
-	        frame.setJMenuBar(demo.createMenuBar());
+	        menu m = new menu();
+	        frame.setJMenuBar(m.createMenuBar());
 	        frame.setContentPane(demo.createContentPane());
+	        frame.add(new ComboBox());
 
 	        //Display the window.
 	        frame.setSize(450, 260);
@@ -132,17 +48,14 @@ public class Program {
         return contentPane;
     }
 
-    /** Returns an ImageIcon, or null if the path was invalid. */
-    protected static ImageIcon createImageIcon(String path) {
-        java.net.URL imgURL = Program.class.getResource(path);
-        if (imgURL != null) {
-            return new ImageIcon(imgURL);
-        } else {
-            System.err.println("Couldn't find file: " + path);
-            return null;
-        }
+
+    // Returns just the class name -- no package info.
+    protected String getClassName(Object o) {
+        String classString = o.getClass().getName();
+        int dotIndex = classString.lastIndexOf(".");
+        return classString.substring(dotIndex+1);
     }
-	
+
 	public static void main(String[] args) {
 	    //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
