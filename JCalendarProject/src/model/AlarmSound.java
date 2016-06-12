405efe845de8d53284430819b2cbed9a3676e1af
MiszelHub <nolanb.o.s@gmail.com>
2016-06-12 17:04:37 +0200

    dodałem klase do odtwarzania dźwieku

---
+++ JCalendarProject/src/model/AlarmSound.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
+public class AlarmSound {
+	private String filePath;
+	private InputStream in;
+	private AudioStream audioStream;
+
+	public AlarmSound(String path){
+		this.filePath = path;
+		try {
+			this.in = new FileInputStream(path);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			this.audioStream = new AudioStream(in);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void playAlarm()
+	{
+		AudioPlayer.player.start(this.audioStream);
+	}
+	public void stopAlarm(){
+		AudioPlayer.player.stop(this.audioStream);
+	}
+}
